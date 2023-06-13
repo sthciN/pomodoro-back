52a9ac47a887fac8d8a9229e87f01bbd174b02c4
Ati <atefeh.barzideh@hotmail.com>
2023-06-14 00:57:26 +0300

    Update user schemas

--- model/schemas.py
+++ model/schemas.py
@@ -1,14 +1,24 @@
 from beanie import PydanticObjectId
 from fastapi_users import schemas
-
+from pydantic import EmailStr
+from typing import Union
 
 class UserRead(schemas.BaseUser[PydanticObjectId]):
-    pass
+    name: str
+    username: str
+    email: EmailStr
+    avatar: Union[str, None] = None
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    name: str
+    username: str
+    email: EmailStr
+    avatar: Union[str, None] = None
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+    name: str
+    username: str
+    email: EmailStr
+    avatar: Union[str, None] = None
